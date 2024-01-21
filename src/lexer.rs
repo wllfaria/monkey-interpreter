d39db4ba5d383b3695b03e8ce07da745092b671f
Willians Faria <dev.willians.faria@gmail.com>
2024-01-21 15:39:12 -0300

    feat: lexer now parse identifiers and integers

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::token;
+use crate::token::*;
 
 struct Lexer {
     input: &'static str,
@@ -28,21 +28,63 @@ impl Lexer {
         self.next += 1;
     }
 
-    fn next_token(&mut self) -> token::Token {
+    fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
         let token = match self.ch {
-            Some('=') => token::Token::new(token::TokenKind::Assign, self.ch.unwrap().to_string()),
-            Some(';') => token::Token::new(token::TokenKind::Semi, self.ch.unwrap().to_string()),
-            Some('(') => token::Token::new(token::TokenKind::LParen, self.ch.unwrap().to_string()),
-            Some(')') => token::Token::new(token::TokenKind::RParen, self.ch.unwrap().to_string()),
-            Some(',') => token::Token::new(token::TokenKind::Comma, self.ch.unwrap().to_string()),
-            Some('+') => token::Token::new(token::TokenKind::Plus, self.ch.unwrap().to_string()),
-            Some('{') => token::Token::new(token::TokenKind::LBrace, self.ch.unwrap().to_string()),
-            Some('}') => token::Token::new(token::TokenKind::RBrace, self.ch.unwrap().to_string()),
-            None => token::Token::new(token::TokenKind::Eof, "".to_string()),
-            _ => token::Token::new(token::TokenKind::Illegal, "".to_string()),
+            Some('=') => Token::new(TokenKind::Assign, self.ch.unwrap().to_string()),
+            Some(';') => Token::new(TokenKind::Semi, self.ch.unwrap().to_string()),
+            Some('(') => Token::new(TokenKind::LParen, self.ch.unwrap().to_string()),
+            Some(')') => Token::new(TokenKind::RParen, self.ch.unwrap().to_string()),
+            Some(',') => Token::new(TokenKind::Comma, self.ch.unwrap().to_string()),
+            Some('+') => Token::new(TokenKind::Plus, self.ch.unwrap().to_string()),
+            Some('{') => Token::new(TokenKind::LBrace, self.ch.unwrap().to_string()),
+            Some('}') => Token::new(TokenKind::RBrace, self.ch.unwrap().to_string()),
+            Some(ch) if ch.is_alphabetic() => {
+                let identifier = self.read_identifier();
+                match identifier.as_str() {
+                    "let" => Token::new(TokenKind::Let, identifier),
+                    "fn" => Token::new(TokenKind::Function, identifier),
+                    _ => Token::new(TokenKind::Ident, identifier),
+                }
+            }
+            Some(c) if c.is_numeric() => Token::new(TokenKind::Int, self.read_integer()),
+            None => Token::new(TokenKind::Eof, "".to_string()),
+            _ => Token::new(TokenKind::Illegal, "".to_string()),
         };
         self.read_char();
         token
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let start = self.position - 1;
+        while self
+            .input
+            .chars()
+            .nth(self.next - 1)
+            .unwrap()
+            .is_alphabetic()
+        {
+            self.read_char();
+        }
+        self.input[start..self.position].to_string()
+    }
+
+    fn read_integer(&mut self) -> String {
+        let start = self.position - 1;
+        while self.input.chars().nth(self.next - 1).unwrap().is_numeric() {
+            self.read_char();
+        }
+        self.input[start..self.position].to_string()
+    }
+
+    fn skip_whitespace(&mut self) {
+        match self.ch {
+            None => return,
+            _ => {}
+        };
+        while self.ch.unwrap().is_whitespace() {
+            self.read_char();
+        }
     }
 }
 
@@ -53,20 +95,52 @@ mod test {
 
     #[test]
     fn test_next_token() {
-        let input = "=+(){},;";
-        let expected: Vec<token::Token> = vec![
-            token::Token::new(token::TokenKind::Assign, "=".to_string()),
-            token::Token::new(token::TokenKind::Plus, "+".to_string()),
-            token::Token::new(token::TokenKind::LParen, "(".to_string()),
-            token::Token::new(token::TokenKind::RParen, ")".to_string()),
-            token::Token::new(token::TokenKind::LBrace, "{".to_string()),
-            token::Token::new(token::TokenKind::RBrace, "}".to_string()),
-            token::Token::new(token::TokenKind::Comma, ",".to_string()),
-            token::Token::new(token::TokenKind::Semi, ";".to_string()),
-            token::Token::new(token::TokenKind::Eof, "".to_string()),
+        let input = "let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+                x + y;
+            };
+            let result = add(five, ten);";
+        let expected: Vec<Token> = vec![
+            Token::new(TokenKind::Let, "let".to_string()),
+            Token::new(TokenKind::Ident, "five".to_string()),
+            Token::new(TokenKind::Assign, "=".to_string()),
+            Token::new(TokenKind::Int, "5".to_string()),
+            Token::new(TokenKind::Semi, ";".to_string()),
+            Token::new(TokenKind::Let, "let".to_string()),
+            Token::new(TokenKind::Ident, "ten".to_string()),
+            Token::new(TokenKind::Assign, "=".to_string()),
+            Token::new(TokenKind::Int, "10".to_string()),
+            Token::new(TokenKind::Semi, ";".to_string()),
+            Token::new(TokenKind::Let, "let".to_string()),
+            Token::new(TokenKind::Ident, "add".to_string()),
+            Token::new(TokenKind::Assign, "=".to_string()),
+            Token::new(TokenKind::Function, "fn".to_string()),
+            Token::new(TokenKind::LParen, "(".to_string()),
+            Token::new(TokenKind::Ident, "x".to_string()),
+            Token::new(TokenKind::Comma, ",".to_string()),
+            Token::new(TokenKind::Ident, "y".to_string()),
+            Token::new(TokenKind::RParen, ")".to_string()),
+            Token::new(TokenKind::LBrace, "{".to_string()),
+            Token::new(TokenKind::Ident, "x".to_string()),
+            Token::new(TokenKind::Plus, "+".to_string()),
+            Token::new(TokenKind::Ident, "y".to_string()),
+            Token::new(TokenKind::Semi, ";".to_string()),
+            Token::new(TokenKind::RBrace, "}".to_string()),
+            Token::new(TokenKind::Semi, ";".to_string()),
+            Token::new(TokenKind::Let, "let".to_string()),
+            Token::new(TokenKind::Ident, "result".to_string()),
+            Token::new(TokenKind::Assign, "=".to_string()),
+            Token::new(TokenKind::Ident, "add".to_string()),
+            Token::new(TokenKind::LParen, "(".to_string()),
+            Token::new(TokenKind::Ident, "five".to_string()),
+            Token::new(TokenKind::Comma, ",".to_string()),
+            Token::new(TokenKind::Ident, "ten".to_string()),
+            Token::new(TokenKind::RParen, ")".to_string()),
+            Token::new(TokenKind::Semi, ";".to_string()),
         ];
         let mut lexer = Lexer::new(input);
-        let mut tokens: Vec<token::Token> = Vec::new();
+        let mut tokens: Vec<Token> = Vec::new();
         for _ in &expected {
             tokens.push(lexer.next_token());
         }
